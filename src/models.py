a1f14dfbb102ba4ff060b1ceb5be3058ec34b76f
Alejandro Gonzalez <ajgonzale@gmail.com>
2024-07-26 16:54:03 +0000

    update the model, adding tables and attributes

--- src/models.py
+++ src/models.py
@@ -1,29 +1,52 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
+from sqlalchemy.types import DECIMAL
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(50), nullable=False)
+    firstname = Column(String(100))
+    lastname = Column(String(100))
+    email = Column(String(300), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Vehicule(Base):
+    __tablename__ = 'vehicule'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    model = Column(String(100))
+    weight = Column(DECIMAL)
+    year = Column(Integer)
+    make = Column(String(100))
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    dob = Column(Date)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    population_in_millions = Column(Integer)
+    terrain = Column(String)
+
+class Favorites(Base) :
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    vehicule_id = Column(Integer, ForeignKey('vehicule.id'))
+    planet = relationship(Planet)
+    character = relationship(Character)
+    vehicule = relationship(Vehicule)
 
     def to_dict(self):
         return {}
